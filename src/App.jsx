05b4d2662984fdb61f00c175f2a10132b2e91a54
AJ <joysondsouzaaj@gmail.com>
2023-06-29 11:30:41 +0530

    css fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-
 import './App.css'
 import Navbar from "./Components/Navbar.jsx";
+import Otp_Login from "./Components/Otp_Login.jsx";
 
 function App() {
 
 
-  return (
-    <>
-      <Navbar />
-    </>
-  )
+    return (
+        <>
+            <Navbar/>
+
+
+        </>
+    )
 }
 
 export default App
