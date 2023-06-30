ba33848cb51b01aeab2122383aebbde3023debb7
shettypri <prishetty1976@gmail.com>
2023-06-30 19:14:37 +0530

    Routes update

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,17 @@ import {BrowserRouter, Route, Routes,} from 'react-router-dom'
 
 import './App.css'
 import Navbar from "./Components/Navbar.jsx";
+
+import Add_ads from "./Components/Admin/Add_ads.jsx";
+import Dashboard from "./Components/Admin/Dashboard.jsx";
+import New_Request from "./Components/Admin/New_Request.jsx";
+import New_user from "./Components/Admin/New_user.jsx";
+import Payment_request from "./Components/Admin/Payment_request.jsx";
 import Otp_Login from "./Components/Otp_Login.jsx";
-import Register from './Components/Users/Register';
-import user from './Components/Users/User_dashboard.jsx'
-import admin from './Components/Admin/Dashboard.jsx'
+import Profile from "./Components/Users/Profile.jsx";
+import Register from "./Components/Users/Register.jsx";
+import User_dashboard from "./Components/Users/User_dashboard.jsx";
+import Waiting from "./Components/Users/Waiting.jsx";
 
 function App() {
 
@@ -17,12 +24,27 @@ function App() {
                  <Navbar/>
                 </div>
                 <Routes>
-                    <Route exact path='/register' element={<Register/>}/>
-                    <Route exact path='/user' element={<user/>}/>
-                    <Route exact path='/admin' element={<admin/>}/>
 
-                    <Route exact path='/admin/otplogin' element={<Otp_Login/>}/>
-                    <Route exact path='/user/otplogin' element={<Otp_Login/>}/>
+                    {/*otp_login*/}
+                    <Route exact path='/otplogin' element={<Otp_Login/>}/>
+
+
+                    {/*admin*/}
+                    <Route exact path='/admin/adds' element={<Add_ads/>}></Route>
+                    <Route exact path='/admin/dashboard' element={<Dashboard/>}></Route>
+                    <Route exact path='/admin/newrequest' element={<New_Request/>}></Route>
+                    <Route exact path='/admin/newuser' element={<New_user/>}></Route>
+                    <Route exact path='/admin/paymentrequest' element={<Payment_request/>}></Route>
+
+
+                    {/*user*/}
+                    <Route exact path ='/user/profile' element={<Profile/>}/>
+                    <Route exact path ='/user/register' element={<Register/>}/>
+                    <Route exact path ='/user/dashboard' element={<User_dashboard/>}/>
+                    <Route exact path ='/user/waiting' element={<Waiting/>}/>
+
+
+
                 </Routes>
 
             </BrowserRouter>
