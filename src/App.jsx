1855a6947c2551d55668db3da8f418534e956dc8
shettypri <prishetty1976@gmail.com>
2023-06-30 18:15:02 +0530

    Routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,28 @@ import './App.css'
 import Navbar from "./Components/Navbar.jsx";
 import Otp_Login from "./Components/Otp_Login.jsx";
 import Register from './Components/Users/Register';
+import user from './Components/Users/User_dashboard.jsx'
+import admin from './Components/Admin/Dashboard.jsx'
 
 function App() {
 
 
     return (
         <>
-            <Navbar/>
-            <New_Request />
+            <BrowserRouter>
+                <div>
+                 <Navbar/>
+                </div>
+                <Routes>
+                    <Route exact path='/register' element={<Register/>}/>
+                    <Route exact path='/user' element={<user/>}/>
+                    <Route exact path='/admin' element={<admin/>}/>
+
+                    <Route exact path='/admin/otplogin' element={<Otp_Login/>}/>
+                    <Route exact path='/user/otplogin' element={<Otp_Login/>}/>
+                </Routes>
+
+            </BrowserRouter>
 
 
         </>
