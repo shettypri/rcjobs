20c81598fd3e75000a027a140a5a48e633827527
AJ <joysondsouzaaj@gmail.com>
2023-07-01 13:14:56 +0530

    register form Fixed

--- src/Components/Users/Register.jsx
+++ src/Components/Users/Register.jsx
@@ -6,12 +6,56 @@ import { setDoc,doc } from "firebase/firestore"
 import {db} from "../../config/firebase.config.js"
 import {useNavigate} from "react-router-dom";
 const Register = () => {
-  return (
-    <>
-        <div className={"flex flex-wrap justify-center mt-4"}>
+    const [uploadImage, setUploadImage] = useState("");
+    const [userDetails, setUserDetails] = useState({
+        id:sessionStorage.getItem("key"),
+        name: "",
+        phone: sessionStorage.getItem("mobileNumber"),
+        limit: 25,
+        referred: 0,
+        isWithdrawing: false,
+        isAdmin: false,
+        isUserAuthorized:false,
+        withdrawalAmount: 0,
+        joining_code:"jguygu",
+        Referral_Code: "admin001"
 
-           <div className={"flex flex-col space-y-3 p-0  bg-white rounded-lg shadow-xl border-2 border-black"}>
-               <div className={" text-3xl font-semibold bg-[#FD7F2C] text-white w-full uppercase m-[0px] rounded  h-12"}>Registration Form</div>
+    });
+    const Navigate = useNavigate()
+    // useEffect(() => {
+    //     sessionStorage && setUserDetails({...userDetails,["id"]:}
+    // }, []);
+    const handleRegister = async ()=>{
+        console.log(uploadImage.name)
+        const imageFile = uploadImage.name
+        const imageFolder = "PAYMENT"
+        const textV4 = v4()
+        //     image uploade code to firebase:
+        const fileFolderRef = ref(storage,`${imageFolder}/${imageFile + textV4}`)
+        try {
+            await uploadBytes(fileFolderRef,uploadImage)
+            const imageUrl = await getDownloadURL(ref(storage,`${imageFolder}/${imageFile + textV4}`))
+            userDetails["JoiningFee"] = imageUrl
+            const isDataInserted = await setDoc(doc(db, "users", `${userDetails.id}`)
+                ,userDetails)
+            Navigate("/user/dashboard")
+            //     storing into fire store data
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+
+    return (
+        <>
+            <div className={"flex flex-wrap justify-center mt-4"}>
+
+                <div className={"flex flex-col space-y-3 p-0  bg-white rounded-lg shadow-xl border-2 border-black"}>
+                    <div
+                        className={" text-3xl font-semibold bg-[#FD7F2C] text-white w-full uppercase m-[0px] rounded  h-12"}>Registration
+                        Form
+                    </div>
 
                     <div className={" flex flex-col text-black p-4"}>
                         <label className={"font-bold"}>Enter Full Name</label>
@@ -60,7 +104,7 @@ const Register = () => {
                     <div className={"flex justify-center p-6 "}>
                         <button
                             className={"text-center font-semibold border-1  text-white border-black rounded-2xl w-[130px] h-[35px] bg-gray-600 hover:bg-orange-400  "}
-                        onClick={handleRegister}
+                            onClick={handleRegister}
                         >
                             Register
                         </button>
