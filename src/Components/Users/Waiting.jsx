5c6a7a57f0895f4b71fdfbaf69fea155db182d50
shettypri <prishetty1976@gmail.com>
2023-07-01 12:12:07 +0530

    waiting page updated

--- src/Components/Users/Waiting.jsx
+++ src/Components/Users/Waiting.jsx
@@ -1,6 +1,13 @@
 const Waiting = () => {
     return (
-        <div>Waiting</div>
+        <>
+            <div className={" flex justify-center text-center p-4 bg-white rounded-lg shadow-xl border-1 border-black mt-10 h-15 w-[250px] ml-20 md:mt-[150px] md:ml-[350px] md:w-[450px] bg-orange-300"}>
+                <p className={"md:text-2xl text-sm font-bold uppercase animate-pulse"}>
+                    please wait till registration process is completed
+                </p>
+            </div>
+
+        </>
     )
 }
 
