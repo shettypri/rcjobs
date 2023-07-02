7afe619c07789c6092e64dec80954bc4a25e2850
AJ <joysondsouzaaj@gmail.com>
2023-07-02 14:47:12 +0530

    user Dashboard done......

--- src/App/Store.js
+++ src/App/Store.js
@@ -2,12 +2,14 @@ import {configureStore} from "@reduxjs/toolkit";
 import userSlice from "./Slice/userSlice.js";
 import adminUserSlice from "./Slice/adminUserSlice.js";
 import adminPaymentSlice from "./Slice/adminPaymentSlice.js";
+import userDashBoardSlice from "./Slice/userDashBoardSlice.js";
 
 const store = configureStore({
     reducer: {
         userReducer: userSlice,
         adminUserReducers:adminUserSlice,
         adminPaymentReducers:adminPaymentSlice,
+        userDashBoardReducers:userDashBoardSlice,
     }
 })
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,37 +1,57 @@
 import logo from "../assets/Logo/rc-jobs-test-logo.png"
-import {Link, useNavigate} from "react-router-dom";
+import {Link, useLocation, useNavigate} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
 import {isLogOutReducers} from "../App/Slice/userSlice.js";
+import {useEffect} from "react";
 
 
 const Navbar = () => {
     const navigate = useNavigate()
     const Dispatch = useDispatch()
+    // const locationPath = useLocation()
+    const {loading, isLoggedIn, newUser, error, data} = useSelector(state => state.userReducer)
+
     const handleLogOut = () => {
         Dispatch(isLogOutReducers())
         sessionStorage && sessionStorage.clear()
         navigate("/")
     }
-    const {isLoggedIn} = useSelector(state => state.userReducer)
     return (
         <nav className="w-full h-14 bg-slate-400 flex justify-between px-4 md:px-4 items-center">
             <div>
                 <img className="h-12 w-25 cursor-pointer" src={logo} alt=""/>
             </div>
             <ul className={" hidden md:flex dspace-x-5 font-bold"}>
-
-                <li className={"mx-[10px] cursor-pointer text-white"}>
-                    <Link to="/dashboard"> dashboard</Link>
-                </li>
-                <li className={"mx-[10px] cursor-pointer text-white"}>
-                    <Link to="/profile"> profile </Link>
-                </li>
                 {
-                    !(isLoggedIn) && (
-                        <li className={"mx-[10px] cursor-pointer text-white"}>
-                            <p onClick={handleLogOut}
-                            > logout</p>
-                        </li>
+                    isLoggedIn && (
+                        <>
+                            {
+                            data.isUserAuthorized ? (
+                                <>
+                                    <li className={"mx-[10px] cursor-pointer text-white"}>
+                                        <Link to="/user/userdashboard"> dashboard</Link>
+                                    </li>
+                                    <li className={"mx-[10px] cursor-pointer text-white"}>
+                                        <Link to="/user/profile"> {data.name} </Link>
+                                    </li>
+                                </>
+                            ) : (
+                                <>
+                                    <li className={"mx-[10px] cursor-pointer text-white"}>
+                                        <Link to="/admin/dashboard"> Admin dashboard</Link>
+                                    </li>
+                                    <li className={"mx-[10px] cursor-pointer text-white"}>
+                                        <Link to="/user/profile"> {data.name} </Link>
+                                    </li>
+                                </>
+                            )
+                        }
+                            <li className={"mx-[10px] cursor-pointer text-white"}>
+                                <p onClick={handleLogOut}
+                                > logout</p>
+                            </li>
+
+                        </>
                     )
                 }
 
