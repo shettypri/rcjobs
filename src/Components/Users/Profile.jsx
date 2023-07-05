1703fcfb6a548c99fa88856c5e8a7c8c8be4ab7c
shettypri <prishetty1976@gmail.com>
2023-07-05 22:24:06 +0530

    user profile changes

--- src/Components/Users/Profile.jsx
+++ src/Components/Users/Profile.jsx
@@ -1,42 +1,64 @@
 import wallet from "../../../src/assets/Images/wallet.png"
  import profile from "../../../src/assets/Images/profile.png"
+import {useState} from "react";
 const Profile = () => {
+    const [withdraw, setWithdraw] = useState(true);
+    const handleChange=(e)=>{
+        if(e.target.value>=250){
+            {setWithdraw(false)}
+        }
+        else{
+            {
+                setWithdraw(true)
+            }
+        }
+    }
+
 
 
     return (
         <>
 
 
-                <div className={" bg-gradient-to-r from-amber-600 to-rose-500 flex flex-col justify-center text-center p-4  rounded-2xl  shadow-xl border-3 border-white mt-10 w-[200px] ml-[100px] md:h-15 md:w-[250px] md:ml-[520px] space-y-11"}>
-                    <div className={" space-y-3"}>
-                        <div className={"flex flex-row justify-evenly space-x-3 "}>
-                            <div className={"border-white border w-9/12 text-center "}>
-                                <img src={profile} height={"90px"} width={"70px"}
-                                     className={""}
-                                />
-                                Name
-                            </div>
-                            <div className={"border-white border w-9/12 "}>
-                                <img src={wallet} height={"90px"} width={"70px"}
-                                     className={"flex flex-col align-middle"}
-                                />
 
-                                balance amount
+                <div className={"  flex flex-col justify-center text-center p-4  rounded-2xl  shadow-xl border-2 border-gray-700 mt-10 w-[200px] ml-[100px] md:h-15 md:w-[250px] md:ml-[520px] space-y-11"}>
+
+                        <div className={" space-y-3"}>
+
+                            <div className={"flex flex-row justify-evenly space-x-3 "}>
+                                <div className={"border-white border w-9/12 text-center "}>
+                                    <img src={profile} height={"90px"} width={"70px"}
+                                         className={""}
+                                    />
+                                    Name
+                                </div>
+                                <div className={"border-white border w-9/12 "}>
+                                    <img src={wallet} height={"90px"} width={"70px"}
+                                         className={"flex flex-col align-middle"}
+                                    />
+
+                                    balance amount
+                                </div>
                             </div>
-                        </div>
                             <div className={"flex flex-col justify-center"}>
                                 <label>enter the amount</label>
                                 <input type={"text"}
-                                className={"md:w-[220px]   bg-gradient-to-r from-yellow-200 to-rose-700 rounded"}/>
+                                       className={"md:w-[220px] rounded-2xl border border-black"}
+                                       onChange={handleChange}/>
+
                             </div>
 
-
-                        <div className={"flex flex-row p-4 justify-evenly   space-x-3"}>
-                             <button className={"bg-yellow-100 hover:bg-yellow-500 md:h-9  w-[150px] rounded-2xl md:w-[155px] h-9 "}>Withdraw</button>
-                            <button className={"bg-yellow-100 hover:bg-red-600 md:h-9 rounded-2xl md:w-[155px] w-[100px]"}>cancel</button>
+                            {/*{}*/}
 
                         </div>
-                    </div>
+
+
+                    <div className={"flex flex-row p-4 justify-evenly   space-x-3"}>
+                            <button className={"disabled:bg-yellow-100 enabled:bg-orange-600 md:h-9  w-[150px] rounded-2xl md:w-[155px] h-9 "} disabled={withdraw}> Withdraw</button>
+                            <button className={"bg-yellow-100 hover:bg-red-700 md:h-9 rounded-2xl md:w-[155px] w-[100px]"}>cancel</button>
+
+                        </div>
+
 
 
 
--- src/Components/Users/Register.jsx
+++ src/Components/Users/Register.jsx
@@ -1,42 +1,59 @@
 import {useEffect, useState} from "react";
 import {v4} from "uuid";
-import { getDownloadURL,  ref, uploadBytes } from "firebase/storage";
+import {getDownloadURL, ref, uploadBytes} from "firebase/storage";
 import {storage} from "../../config/firebase.config.js";
-import { setDoc,doc } from "firebase/firestore"
+import {setDoc, doc} from "firebase/firestore"
 import {db} from "../../config/firebase.config.js"
 import {useNavigate} from "react-router-dom";
+
 const Register = () => {
     const [uploadImage, setUploadImage] = useState("");
+    const [personDetails, setPersonalDetails] = useState(true);
+    const [final, setFinal] = useState(true);
+
     const [userDetails, setUserDetails] = useState({
-        id:sessionStorage.getItem("key"),
+        id: sessionStorage.getItem("key"),
         name: "",
         phone: sessionStorage.getItem("mobileNumber"),
         limit: 25,
         referred: 0,
-        wallet:0,
+        wallet: 0,
         isWithdrawing: false,
         isAdmin: false,
-        isUserAuthorized:false,
+        isUserAuthorized: false,
         withdrawalAmount: 0,
-        joining_code:"jguygu",
-        Referral_Code: "admin001"
+        joining_code: "jguygu",
+        Referral_Code: "admin001",
+        Account_name: "",
+        Account_no: "",
+        Bank_name: "",
+        Branch: "",
+        ifsc_code: "",
+        Address: ""
+
 
     });
     const Navigate = useNavigate()
+    const uservalues = (e) => {
+        userDetails({
+            ...userDetails, [e.target.name]: e.target.value
+        })
+    }
 
-    const handleRegister = async ()=>{
+
+    const handleRegister = async () => {
         console.log(uploadImage.name)
         const imageFile = uploadImage.name
         const imageFolder = "PAYMENT"
         const textV4 = v4()
         //     image uploade code to firebase:
-        const fileFolderRef = ref(storage,`${imageFolder}/${imageFile + textV4}`)
+        const fileFolderRef = ref(storage, `${imageFolder}/${imageFile + textV4}`)
         try {
-            await uploadBytes(fileFolderRef,uploadImage)
-            const imageUrl = await getDownloadURL(ref(storage,`${imageFolder}/${imageFile + textV4}`))
+            await uploadBytes(fileFolderRef, uploadImage)
+            const imageUrl = await getDownloadURL(ref(storage, `${imageFolder}/${imageFile + textV4}`))
             userDetails["JoiningFee"] = imageUrl
             const isDataInserted = await setDoc(doc(db, "users", `${userDetails.id}`)
-                ,userDetails)
+                , userDetails)
             Navigate("/user/dashboard")
             //     storing into fire store data
 
@@ -50,64 +67,174 @@ const Register = () => {
         <>
             <div className={"flex flex-wrap justify-center mt-4"}>
 
-                <div className={"flex flex-col space-y-3 p-0  bg-white rounded-lg shadow-xl border-2 border-black"}>
-                    <div
-                        className={" text-3xl font-semibold bg-[#FD7F2C] text-white w-full uppercase m-[0px] rounded  h-12"}>Registration
-                        Form
-                    </div>
+                <div className="flex flex-col space-y-3 p-0  bg-white rounded-lg shadow-xl border-2 border-black
+                py-10
+                ">
 
-                    <div className={" flex flex-col text-black p-4"}>
-                        <label className={"font-bold"}>Enter Full Name</label>
-                        <input
-                            type="text"
-                            className=" mt-1 border-2 border-black h-10 rounded
-                    font-bold bg-gray-200 shadow-gray-500 shadow-xl
+                    {personDetails ?(
+                        <div>
+                            <div
+                                className={" text-3xl font-semibold bg-[#FD7F2C] text-white w-full uppercase m-[0px] mt-[0px] rounded  h-15"}>Registration
+                                Form
+                            </div>
+
+                            <div className={" flex flex-col text-black p-4"}>
+                                <label className={"font-bold"}>Enter Full Name</label>
+                                <input
+                                    type="text"
+                                    className=" mt-1 border-2 border-black h-10 rounded
+                    font-bold   shadow-xl
                     "
-                            name={"name"} onChange={(event) => {
-                            setUserDetails({
-                                ...userDetails,["name"]:event.target.value
-                            })
-                        }}/>
-                    </div>
+                                    name={"name"} onChange={(event) => {
+                                    setUserDetails({
+                                        ...userDetails, ["name"]: event.target.value
+                                    })
+                                }}/>
+                            </div>
 
-                    <div className={" flex flex-col text-black p-4"}>
-                        <label className={"font-bold"}> phone number</label>
-                        <input
-                            type="text"
-                            value={userDetails.phone}
-                            disabled
-                            className=" mt-1 border-2 border-gray-700 h-10 rounded font-bold
-                       bg-gray-200 shadow-gray-500 shadow-xl
+                            <div className={" flex flex-col text-black p-4"}>
+                                <label className={"font-bold"}> phone number</label>
+                                <input
+                                    type="text"
+                                    value={userDetails.phone}
+                                    disabled
+                                    className=" mt-1 border-2 border-gray-700 h-10 rounded font-bold
+                       shadow-xl
                        "/>
-                    </div>
-
-                    <div className={" flex flex-col text-black p-4"}>
-                        <label className={"font-bold"}>Enter payment proof </label>
-                        <div className="">
-                            <input
-                                type="file"
-                                name="name"
-                                accept="image/*"
-                                className=" mt-1  "
-                                onChange={(event)=>{
-                                    setUploadImage(event.target.files[0])
-                                }}
-                            />
+                            </div>
+                            <div className="flex flex-row justify-center">
+                                <button
+                                   className={ "text-center font-semibold border-1  text-white border-black rounded-2xl w-[120px] h-[35px] bg-gray-600 hover:bg-orange-400 "}
+                                onClick={()=>setPersonalDetails(false)}>Next
+                                </button>
+                            </div>
                         </div>
-                    </div>
-                    <div className={"p-6"}>
-                        IF NOT PAID YET <span
-                        className={"italic text-blue-700 underline underline-offset-1 cursor-pointer hover:text-blue-900"}> click here to pay</span>
-                    </div>
+                    ):(
+                        <div>
+                            {final ?(
+                                <div>
+                                    <div className={"flex flex-col text-black p-4"}>
 
-                    <div className={"flex justify-center p-6 "}>
-                        <button
-                            className={"text-center font-semibold border-1  text-white border-black rounded-2xl w-[130px] h-[35px] bg-gray-600 hover:bg-orange-400  "}
-                            onClick={handleRegister}
-                        >
-                            Register
-                        </button>
-                    </div>
+                                        <div className={"flex flex-col justify-center"}>
+                                            <label className={"font-bold"}>Name</label>
+                                            <input
+                                                className={"mt-1 border-2 border-black h-10 rounded font-bold  shadow-xl"}
+                                                type={"text"}
+                                                name={"name"}
+                                                onChange={uservalues}
+                                            />
+                                        </div>
+                                        <div className={"flex flex-col justify-center"}>
+                                            <label className={"font-bold"}>Account number</label>
+                                            <input
+                                                className={"mt-1 border-2 border-black h-10 rounded font-bold  shadow-xl"}
+                                                type={"text"}
+                                                name={"name"}
+                                                onChange={uservalues}/>
+                                        </div>
+                                        <div className={"flex flex-col justify-center"}>
+                                            <label className={"font-bold"}> Bank Name</label>
+                                            <input
+                                                className={"mt-1 border-2 border-black h-10 rounded font-bold  shadow-xl"}
+                                                type={"text"}
+                                                name={"name"}
+                                                onChange={uservalues}/>
+                                        </div>
+                                        <div className={"flex flex-col justify-center"}>
+                                            <label className={"font-bold"}> Branch</label>
+                                            <input
+                                                className={"mt-1 border-2 border-black h-10 rounded font-bold  shadow-xl"}
+                                                type={"text"}
+                                                name={"name"}
+                                                onChange={uservalues}/>
+                                        </div>
+                                        <div className={"flex flex-col justify-center"}>
+                                            <label className={"font-bold"}> IFSC code</label>
+                                            <input
+                                                className={"mt-1 border-2 border-black h-10 rounded font-bold  shadow-xl"}
+                                                type={"text"}
+                                                name={"name"}
+                                                onChange={uservalues}/>
+                                        </div>
+                                        <div className={"flex flex-col justify-center"}>
+                                            <label className={"font-bold"}> Address</label>
+                                            <input
+                                                className={"mt-1 border-2 border-black h-[60px] rounded font-bold shadow-xl "}
+                                                type={"text"}
+                                                name={"name"}
+                                                onChange={uservalues}/>
+
+                                        </div>
+
+                                        <div className={"flex justify-center p-6 flex-row space-x-3 "}>
+                                            <button
+                                                className={"\"text-center font-semibold border-1  text-white border-black rounded-2xl w-[120px] h-[35px] bg-gray-600 hover:bg-orange-400 "}
+                                                onClick={()=>setPersonalDetails(true)}
+                                            >
+                                                Back
+                                            </button>
+                                            <button
+                                                className={"\"text-center font-semibold border-1  text-white border-black rounded-2xl w-[120px] h-[35px] bg-gray-600 hover:bg-orange-400 "
+                                                }onClick={()=>setFinal(false)}>Next
+
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            ):(
+                                <div>
+                                    <div>
+                                        <div className={"flex flex-col  mt-4"}>
+                                            <label className={"font-bold flex space-y-3 p-3 justify-center"}>Enter
+                                                payment proof </label>
+                                            <div className="">
+                                                <input
+                                                    type="file"
+                                                    name="name"
+                                                    accept="image/*"
+                                                    className=" mt-1 flex justify-center p-3 space-y-3 "
+                                                    onChange={(event) => {
+                                                        setUploadImage(event.target.files[0])
+                                                    }}
+                                                />
+                                            </div>
+
+                                        </div>
+                                    </div>
+
+                                    <div className={"flex justify-center p-6 flex-row space-x-3 "}>
+                                        <button
+                                            className={"text-center font-semibold border-1  text-white border-black rounded-2xl w-[120px] h-[35px] bg-gray-600 hover:bg-orange-400  "}
+                                            onClick={()=>{setFinal(true)}}
+                                        >
+                                            back
+                                        </button>
+                                        <button
+                                            className={"text-center font-semibold border-1  text-white border-black rounded-2xl w-[120px] h-[35px] bg-gray-600 hover:bg-orange-400  "}
+                                            onClick={handleRegister}
+                                        >
+                                            Register
+                                        </button>
+                                    </div>
+                                </div>
+                            )}
+
+
+
+
+
+                        </div>
+                    )}
+
+
+
+                    {/*<div className={"p-6"}>*/}
+                    {/*    IF NOT PAID YET <span*/}
+                    {/*    className={"italic text-blue-700 underline underline-offset-1 cursor-pointer hover:text-blue-900"}> click here to pay</span>*/}
+                    {/*</div>*/}
+
+
+
 
 
                 </div>
