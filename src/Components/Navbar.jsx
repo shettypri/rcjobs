c10eab6bdb209cb8cb5e93a61acab57f342ab94f
shettypri <prishetty1976@gmail.com>
2023-08-04 11:34:48 +0530

    Navbar update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -32,12 +32,12 @@ const Navbar = () => {
 
 
     return (
-        <nav className="w-full h-14 bg-slate-400 flex justify-between px-4 md:px-4 items-center">
+        <nav className="w-full h-14 bg-slate-600 flex justify-between px-4 md:px-4 items-center">
             <div>
                 <img className="h-12 w-25 cursor-pointer" src={logo} alt=""/>
             </div>
             <div>
-                <ul className="duration-500 font-bold md:static absolute md:min-h-fit min-h-[60vh] left-0 top-[-100%] md:w-auto  w-full flex items-center  flex-col md:flex-row px-5">
+                <ul className=" font-bold md:static absolute md:min-h-fit min-h-[60vh] left-0 top-[-100%] md:w-auto  w-full flex items-center  flex-col md:flex-row px-5">
                     {
                         isLoggedIn && (
                             <>
@@ -93,6 +93,54 @@ const Navbar = () => {
                     &#9776;
                 </section>
 
+                {toggle ? (<div className=" bg-slate-600 group fixed z-50  right-0 flex flex-col pt-2 mt-[-4px] mr-60">
+                    <ul className=" cursor-pointer px-1 mr-20 flex flex-col w-3/4">
+                        {
+                            isLoggedIn && (
+                                <>
+                                    {
+                                        data.isUserAuthorized ? (
+                                            <>
+                                                <li className={"mx-[10px] cursor-pointer text-white my-3 border-b border-white"}>
+                                                    <Link to="/user/userdashboard " className="font-extrabold capitalize "> dashboard</Link>
+                                                </li>
+                                                <li className={"mx-[10px] cursor-pointer text-white my-3 border-b border-white"}>
+                                                    <Link to="/user/withdraw" className="font-extrabold capitalize" > Withdraw</Link>
+                                                </li>
+
+                                                <li className={"mx-[10px] cursor-pointer text-white my-3 border-b border-white"}>
+                                                    <Link to="/user/profile" className="font-extrabold capitalize"> {data.name} </Link>
+                                                </li>
+                                            </>
+                                        ) : (
+                                            <>
+                                                {
+                                                    data.isAdmin && (
+                                                        <>
+                                                            <li className={"mx-[10px] cursor-pointer text-white my-3 border-white border-b"}>
+                                                                <Link to="/admin/dashboard"> Admin dashboard</Link>
+                                                            </li>
+                                                            <li className={"mx-[10px] cursor-pointer text-white"}>
+                                                                <Link to="/user/profile"> {data.name} </Link>
+                                                            </li>
+                                                        </>
+                                                    )
+                                                }
+                                            </>
+                                        )
+                                    }
+                                    <li className={"mx-[10px] cursor-pointer text-white my-3"}>
+                                        <p onClick={handleLogOut} className="font-extrabold capitalize border-b border-white"
+                                        > logout</p>
+                                    </li>
+
+                                </>
+                            )
+                        }
+
+                        {/*<li className={"mx-[10px] cursor-pointer text-white"}> <Link to="/otplogin"> logout </Link> </li>*/}
+                    </ul>
+                </div>):("")}
             </div>
 
 
