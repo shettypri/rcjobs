58f63bde80134288a422c1aefee2bf2ee8e92556
AJ <joysondsouzaaj@gmail.com>
2023-08-09 15:28:26 +0530

    changes

--- src/Components/Company_Bank_Details/Payment_Info.jsx
+++ src/Components/Company_Bank_Details/Payment_Info.jsx
@@ -10,7 +10,7 @@ const PaymentInfo =()=> {
         <div>
             <div className="">
                 <select  value={selectedOption}
-                         className="w-full py-3 border-2 border-black uppercase">
+                         className="w-full py-3 border-2 border-black uppercase mx-auto">
                     <option className="uppercase"
                         onClick={()=>setShowBankDetails(true)}>Bank Payment</option>
                     <option className="uppercase"
