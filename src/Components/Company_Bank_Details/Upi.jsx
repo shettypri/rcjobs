995380dd8eaca5dc9c04e15779a92a6ba6671e89
shettypri <prishetty1976@gmail.com>
2023-08-09 15:21:03 +0530

    qr code added fixed

--- src/Components/Company_Bank_Details/Upi.jsx
+++ src/Components/Company_Bank_Details/Upi.jsx
@@ -4,8 +4,8 @@ import Qr_code from '../../assets/QR_CODE/QRCode_Image.jpeg'
 const Upi = () => {
     return (
         <>
-          <div className="w-[600px] flex   ">
-              <img  className="w-64 " src={Qr_code} alt=""/>
+          <div className="w-[600px] flex align-middle justify-center py-4 ">
+              <img  className="w-80 " src={Qr_code} alt=""/>
           </div>
 
 
