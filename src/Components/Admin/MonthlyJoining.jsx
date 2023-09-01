03523ccc81ccf08d9fa4688b7b8c7833188a8965
AJ <joysondsouzaaj@gmail.com>
2023-09-01 17:31:28 +0530

    fixing has been done

--- src/Components/Admin/MonthlyJoining.jsx
+++ src/Components/Admin/MonthlyJoining.jsx
@@ -1,11 +1,51 @@
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect} from "react";
+import {getCustomerReducers} from "../../App/Slice/AdminCustomerSlice.js";
 
 
 const MonthlyJoining = () => {
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(getCustomerReducers())
+    }, []);
+    const {customerUser} = useSelector(state => state.CustomerReducers)
+    const filterData =customerUser.data.filter(
+        customerData => customerData.Joining_Month === (new Date().toUTCString().slice(5, 16).split(" ")[1])
+    )
     return(
         <>
-            <h1>
-                Monthly users
-            </h1>
+            <div className="flex flex-col bg-gray-800 w-3/4 mx-auto rounded">
+                <div className="flex flex-row justify-around py-4 ">
+                    <label className="text-white text-3xl p-8">Number of users &nbsp;{customerUser.data.length}</label>
+                    <label className="text-white text-3xl border-2 rounded-full p-8"> {(customerUser.data.length)*1000}</label>
+                </div>
+                <div className="w-full ">
+                    <table className="text-white w-full border-2 border-white px-2">
+                        <thead>
+                        <tr>
+                            <th>SI NO</th>
+                            <th>Name</th>
+                            <th>Phone</th>
+                        </tr>
+                        </thead>
+
+                        <tbody className="">
+                        {
+                            filterData.map((newUsers,index)=>{
+                                return(
+                                        <tr>
+                                            <th >{index + 1}</th>
+                                            <th >{newUsers.name}</th>
+                                            <th >{newUsers.phone}</th>
+                                        </tr>
+                                )
+                            })
+                        }
+                        </tbody>
+                    </table>
+
+                </div>
+            </div>
         </>
     );
 }
