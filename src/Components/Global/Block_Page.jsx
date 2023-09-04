c35b3b88eb59d7438501064f5a28639965640ea8
shettypri <prishetty1976@gmail.com>
2023-09-04 11:59:56 +0530

    merge changes

---
+++ src/Components/Global/Block_Page.jsx
@@ -0,0 +1,23 @@
+
+const Block_Page = () => {
+
+    return (
+        <>
+
+
+
+
+            <div role="alert">
+                <div className="bg-red-500 text-white font-bold rounded-t px-4 py-2">
+                    Danger
+                </div>
+                <div className="border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700">
+                    <p>you have been blocked by the Admin</p>
+                </div>
+            </div>
+
+
+        </>
+    )
+}
+export default  Block_Page;
