5abb9d92b3072f38a55531e2ab479547e1778431
AJ <joysondsouzaaj@gmail.com>
2023-09-05 22:07:21 +0530

    Monthly report have been added

--- src/Components/Admin/MonthlyJoining.jsx
+++ src/Components/Admin/MonthlyJoining.jsx
@@ -8,6 +8,8 @@ import Select from 'react-select';
 const MonthlyJoining = () => {
     const dispatch = useDispatch()
     const todayDate = new Date()
+    const [totalAmount, setTotalAmount] = useState(0)
+    const [withdrwalDataArray, setWithdrwalDataArray] = useState("");
     const [selectedMonth, setSelectedMonth] = useState(todayDate.getMonth())
     const [selectedYear, setSelectedYear] = useState(todayDate.getFullYear());
 
@@ -19,9 +21,19 @@ const MonthlyJoining = () => {
     
     const {customerUser} = useSelector(state => state.CustomerReducers)
     const {withdrawalStoreData} = useSelector(state => state.withdrawalReducer)
-    // console.log("HELLO IAHOXOD",customerUser)
-    console.log("dates",withdrawalStoreData.data)
+    // console.log("dates",withdrawalStoreData.data)
     let filterData;
+
+    const reducedArray = (arrayData)=>{
+        console.log(arrayData)
+        let resultArray = arrayData.reduce((prev,current)=>{
+            return prev +current.withdrawalAmount
+        },0)
+        setTotalAmount(resultArray)
+        setWithdrwalDataArray(arrayData)
+        console.log("hello",totalAmount)
+    }
+
     customerUser.success &&(
     filterData = customerUser.data.filter(
         customerData => customerData.payment_Month === (new Date().toUTCString().slice(5, 16).split(" ")[1])
@@ -29,43 +41,50 @@ const MonthlyJoining = () => {
     )
     let withdrwalData;
     withdrawalStoreData.Success &&(
-        withdrwalData = withdrawalStoreData.data.filter(
-            withDrawlInfo => withDrawlInfo.payment_Month === selectedMonth && withDrawlInfo.payment_year === selectedYear
-        )
+            useEffect(() => {
+                withdrwalData = withdrawalStoreData.data.filter(
+                    withDrawlInfo => withDrawlInfo.payment_Month === selectedMonth && withDrawlInfo.payment_year === selectedYear
+                )
+                reducedArray(withdrwalData)
+            }, [])
+    // console.log("AGAIN",withdrwalData)
+
     )
-    console.log("customerdata",filterData)
-    console.log("withdrwalData",withdrwalData)
+    // console.log("customerdata",filterData)
+    // console.log("withdrwalData",withdrwalData)
     const monthOption =[
-        {value: 0, label: 'January'},
-        {value: 1, label: 'February'},
-        {value: 2, label: 'March'},
-        {value: 3, label: 'April'},
-        {value: 4, label: 'May'},
-        {value: 5, label: 'June '},
-        {value: 6, label: 'July'},
-        {value: 7, label: 'August'},
-        {value: 8, label: 'September'},
-        {value: 9, label: 'October'},
-        {value: 10, label: 'November'},
-        {value: 11, label: 'December'},
+        {value: 0, label: 'January'}, {value: 1, label: 'February'},
+        {value: 2, label: 'March'}, {value: 3, label: 'April'},
+        {value: 4, label: 'May'}, {value: 5, label: 'June '},
+        {value: 6, label: 'July'}, {value: 7, label: 'August'},
+        {value: 8, label: 'September'}, {value: 9, label: 'October'},
+        {value: 10, label: 'November'}, {value: 11, label: 'December'},
     ]
 
     const yearOption =[
-        {value: 2023, label: '2023'},
-        {value: 2024, label: '2024'},
-        {value: 2025, label: '2025'},
-        {value: 2026, label: '2026'},
-        {value: 2027, label: '2027'},
-        {value: 2028, label: '2028'},
-        {value: 2029, label: '2029'},
-        {value: 2030, label: '2030'},
+        {value: 2023, label: '2023'}, {value: 2024, label: '2024'},
+        {value: 2025, label: '2025'}, {value: 2026, label: '2026'},
+        {value: 2027, label: '2027'}, {value: 2028, label: '2028'},
+        {value: 2029, label: '2029'}, {value: 2030, label: '2030'},
         {value: 2031, label: '2031'},
     ]
 
     const fetchDataByfilter = ()=>{
+        console.log("month",selectedMonth.value)
+        console.log("year",selectedYear)
+        setWithdrwalDataArray("")
         withdrwalData = withdrawalStoreData.data.filter(
-            withDrawlInfo => withDrawlInfo.payment_Month === selectedMonth && withDrawlInfo.payment_year === selectedYear
-        )
+            withDrawlInfo => withDrawlInfo.payment_Month === selectedMonth.value && withDrawlInfo.payment_year === selectedYear.value
+        ),
+
+            reducedArray(withdrwalData)
+
+    }
+    const handleMonthChange = (selectedOption)=>{
+        setSelectedMonth(selectedOption)
+    }
+    const handleChangeYear =(selectedYear)=>{
+        setSelectedYear(selectedYear)
     }
 
     return (
@@ -76,7 +95,7 @@ const MonthlyJoining = () => {
                     <div className="w-1/2 ">
                         <Select
                             defaultValue={selectedMonth}
-                            onChange={setSelectedMonth}
+                            onChange={handleMonthChange}
                             options={monthOption}
                         />
                     </div>
@@ -84,7 +103,7 @@ const MonthlyJoining = () => {
                     <div className="w-1/2">
                         <Select
                             defaultValue={selectedYear}
-                            onChange={setSelectedYear}
+                            onChange={handleChangeYear}
                             options={yearOption}
                             className="bg-green-600"
                         />
@@ -110,30 +129,40 @@ const MonthlyJoining = () => {
                             className="text-white text-3xl border-2 rounded-full p-8"> {(customerUser.data.length) * 1000}</label>
                     </div>
 
+                    <div className="w-full flex flex-row justify-around py-4  border-2 border-white rounded shadow shadow-amber-500">
+                        <section className="text-white text-3xl uppercase ">
+                            The Withdrawal amount
+                        </section>
+
+                    </div>
+
                     <div className="flex flex-row justify-around py-4 ">
-                        <label className="text-white text-3xl p-8">Number of
-                            users &nbsp;{customerUser.data.length}</label>
+                        <label className="text-white text-3xl p-8">
+                            The withdrawal in this month
+                            </label>
                         <label
-                            className="text-white text-3xl border-2 rounded-full p-8"> {(customerUser.data.length) * 1000}</label>
+                            className="text-white text-3xl border-2 rounded-full p-8"> {totalAmount}</label>
                     </div>
                     <div className="w-full ">
                         <table className="text-white w-full border-2 border-white px-2">
                             <thead>
                             <tr>
-                                <th>SI NO</th>
-                                <th>Name</th>
-                                <th>Phone</th>
+                                <th className="border-2 border-white py-1 capitalize">SI NO</th>
+                                <th className="border-2 border-white py-1 capitalize">Name</th>
+                                <th className="border-2 border-white py-1 capitalize">Phone</th>
+                                <th className="border-2 border-white py-1 capitalize">Amount</th>
                             </tr>
                             </thead>
 
                             <tbody className="">
                             {
-                                customerUser.success && filterData.map((newUsers, index) => {
+                                (withdrwalDataArray.length>0) && withdrwalDataArray.map((newUsers, index) => {
                                     return (
                                         <tr key={index}>
-                                            <th>{index + 1}</th>
-                                            <th>{newUsers.name}</th>
-                                            <th>{newUsers.phone}</th>
+                                            <th className="border-2 border-white py-1 capitalize">{index + 1}</th>
+                                            <th className="border-2 border-white py-1 capitalize">{newUsers.name}</th>
+                                            <th className="border-2 border-white py-1 capitalize">{newUsers.phone}</th>
+                                            <th className="border-2 border-white py-1 capitalize">{newUsers.withdrawalAmount}</th>
                                         </tr>
                                     )
                                 })
