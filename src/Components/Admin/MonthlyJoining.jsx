502d9561523bcda82c8d897d5641020305ee09e5
AJ <joysondsouzaaj@gmail.com>
2023-09-05 17:33:52 +0530

    Changes

--- src/Components/Admin/MonthlyJoining.jsx
+++ src/Components/Admin/MonthlyJoining.jsx
@@ -2,13 +2,15 @@ import {useDispatch, useSelector} from "react-redux";
 import {useEffect, useState} from "react";
 import {getCustomerReducers} from "../../App/Slice/AdminCustomerSlice.js";
 import {withdrawalDataReducers} from "../../App/Slice/WithdrawalSlice.js";
+import Select from 'react-select';
 
 
 const MonthlyJoining = () => {
     const dispatch = useDispatch()
     const todayDate = new Date()
     const [selectedMonth, setSelectedMonth] = useState(todayDate.getMonth())
-    
+    const [selectedYear, setSelectedYear] = useState(todayDate.getFullYear());
+
     useEffect(() => {
         dispatch(getCustomerReducers())
         dispatch(withdrawalDataReducers())
@@ -22,31 +24,84 @@ const MonthlyJoining = () => {
     let filterData;
     customerUser.success &&(
     filterData = customerUser.data.filter(
-        customerData => customerData.Joining_Month === (new Date().toUTCString().slice(5, 16).split(" ")[1])
+        customerData => customerData.payment_Month === (new Date().toUTCString().slice(5, 16).split(" ")[1])
     )
     )
     let withdrwalData;
     withdrawalStoreData.Success &&(
         withdrwalData = withdrawalStoreData.data.filter(
-            withDrawlInfo => withDrawlInfo.payment_Month === new Date().getMonth()
+            withDrawlInfo => withDrawlInfo.payment_Month === selectedMonth && withDrawlInfo.payment_year === selectedYear
         )
     )
-    
+    console.log("customerdata",filterData)
+    console.log("withdrwalData",withdrwalData)
+    const monthOption =[
+        {value: 0, label: 'January'},
+        {value: 1, label: 'February'},
+        {value: 2, label: 'March'},
+        {value: 3, label: 'April'},
+        {value: 4, label: 'May'},
+        {value: 5, label: 'June '},
+        {value: 6, label: 'July'},
+        {value: 7, label: 'August'},
+        {value: 8, label: 'September'},
+        {value: 9, label: 'October'},
+        {value: 10, label: 'November'},
+        {value: 11, label: 'December'},
+    ]
+
+    const yearOption =[
+        {value: 2023, label: '2023'},
+        {value: 2024, label: '2024'},
+        {value: 2025, label: '2025'},
+        {value: 2026, label: '2026'},
+        {value: 2027, label: '2027'},
+        {value: 2028, label: '2028'},
+        {value: 2029, label: '2029'},
+        {value: 2030, label: '2030'},
+        {value: 2031, label: '2031'},
+    ]
+
+    const fetchDataByfilter = ()=>{
+        withdrwalData = withdrawalStoreData.data.filter(
+            withDrawlInfo => withDrawlInfo.payment_Month === selectedMonth && withDrawlInfo.payment_year === selectedYear
+        )
+    }
 
     return (
         <>
             <div className="flex flex-col">
 
-                <div className="w-full">
-                    <label>
-                        Pick a fruit:
-                        <select name="selectedFruit">
-                            <option value="apple">Apple</option>
-                            <option value="banana">Banana</option>
-                            <option value="orange">Orange</option>
-                        </select>
-                    </label>
+                <div className="w-3/4 text-black flex flex-row justify-center mx-auto my-1" >
+                    <div className="w-1/2 ">
+                        <Select
+                            defaultValue={selectedMonth}
+                            onChange={setSelectedMonth}
+                            options={monthOption}
+                        />
+                    </div>
+
+                    <div className="w-1/2">
+                        <Select
+                            defaultValue={selectedYear}
+                            onChange={setSelectedYear}
+                            options={yearOption}
+                            className="bg-green-600"
+                        />
+                    </div>
+
                 </div>
+                <div className="mt-4 w-1/4  flex mx-auto my-2">
+                    <button className="bg-green-600 w-full py-2 border-2 border-black rounded
+                    font-extrabold text-white uppercase
+                    "
+                    onClick={fetchDataByfilter}
+                    >
+                        Display
+                    </button>
+
+                </div>
+
                 <div className="flex flex-col bg-gray-800 w-3/4 mx-auto rounded">
                     <div className="flex flex-row justify-around py-4 ">
                         <label className="text-white text-3xl p-8">Number of
