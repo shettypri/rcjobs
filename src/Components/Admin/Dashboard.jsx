06948f8de893282cb8c6b49e4a34dc66418fa3b1
AJ <joysondsouzaaj@gmail.com>
2023-09-07 11:38:20 +0530

    changes applied

--- src/Components/Admin/Dashboard.jsx
+++ src/Components/Admin/Dashboard.jsx
@@ -24,7 +24,7 @@ const Dashboard = () => {
                 </h1>
 
                 <div className="flex justify-end w-full mr-6 py-2 mb-2">
-                    <button className="border-2 border-black px-4 py-2 rounded-full  uppercase font-extrabold bg-green-900 text-white drop-shadow-md shadow-gray-600"
+                    <button className=" px-4 py-2 rounded  uppercase font-extrabold bg-green-700 text-white drop-shadow-md shadow-gray-600"
                     onClick={()=>navigate('/admin/order')}
                     >
                         Order Request {
