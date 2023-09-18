92045a9f28ae1d7c3df6caa7d033f6ddb52e9d6c
AJ <joysondsouzaaj@gmail.com>
2023-09-18 10:44:14 +0530

    CONSTANT FILE HAS BEEN FIXED

---
+++ src/Constants/database.js
@@ -0,0 +1,4 @@
+export const USERS_COLLECTION = 'users';
+export const PAYMENTS_COLLECTION = 'payments';
+export const BUY_PRODUCTS_COLLECTION = 'BUY_PRODUCT';
+export const ADS_DATA_COLLECTION = "ADS_DATA"
